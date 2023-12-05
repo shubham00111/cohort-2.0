4c2894b291e7c87e1fdffdda3d51c2262e3af145
Shubham Rawat <shubhamb23efd@gmail.com>
2023-12-05 21:38:09 +0530

    completed the hard questions

--- hard/todo-list.js
+++ hard/todo-list.js
@@ -10,6 +10,34 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Todo {}
+class Todo {
+  constructor() {
+    this.todoList = [];
+  }
+
+  add(todo) {
+    this.todoList.push(todo);
+  }
+  outsideLength(index) {
+    return index < 0 || index > this.todoList.length - 1;
+  }
+  remove(index) {
+    return !this.outsideLength(index) ? this.todoList.splice(index, 1) : null;
+  }
+  update(index, updatedTodo) {
+    return !this.outsideLength(index)
+      ? this.todoList.splice(index, 1, updatedTodo)
+      : null;
+  }
+  getAll() {
+    return this.todoList;
+  }
+  get(indexOfTodo) {
+    return !this.outsideLength(indexOfTodo) ? this.todoList[indexOfTodo] : null;
+  }
+  clear() {
+    this.todoList = [];
+  }
+}
 
 module.exports = Todo;
